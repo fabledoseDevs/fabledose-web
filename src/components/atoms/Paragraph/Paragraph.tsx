2603c7ed6882d296b963ebfdbcca9f20a6634d3b
Paweł Modzelewski <last.dunadan@gmail.com>
2025-06-04 15:58:16 +0200

    paragraph component

---
+++ src/components/atoms/Paragraph/Paragraph.tsx
@@ -0,0 +1,13 @@
+import { ParagraphBody } from './Paragraph.styled';
+import type { ParagraphType } from './Paragraph.types';
+import { FOREGROUND_COLOR, TEXT_ALIGNMENT } from './Paragraph.types';
+
+export const Paragraph: ParagraphType = ({
+  color = FOREGROUND_COLOR.PURPLE,
+  alignment = TEXT_ALIGNMENT.LEFT,
+  children,
+}) => (
+  <ParagraphBody foreground={color} textAlign={alignment}>
+    {children}
+  </ParagraphBody>
+);
