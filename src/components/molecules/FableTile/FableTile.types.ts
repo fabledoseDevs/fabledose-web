923629eeb87d012edbb95aa530a21f2c338a35c3
Paweł Modzelewski <last.dunadan@gmail.com>
2025-06-19 11:10:29 +0200

    docs

--- src/components/molecules/FableTile/FableTile.types.ts
+++ src/components/molecules/FableTile/FableTile.types.ts
@@ -2,21 +2,26 @@ import type { ReactElement } from 'react';
 
 /**
  * @file
- * This file defines the types and interfaces for FableTile component.
+ * This file defines the types and interfaces for a FableTile component.
  *
- * The FableTile have following properites.
+ * The FableTile displays a small, interactive cover for a fable.
  *
  * Example usage:
  * ```tsx
- * <FableTile />
+ * <FableTile
+ *   imageUrl={mockImageOne.src}
+ *   fableTitle={'Złotowłosa'}
+ *   fableId={'zlotowlosa-i-trzy-misie'}
+ * />
  * ```
  */
 
 /**
  * Interface for FableTile component props.
  *
- * @property imageUrl - Lorem ipsum...
- * @property fableTitle - Sit amet...
+ * @property imageUrl - Source (url address) for fable cover image.
+ * @property fableTitle - Title of fable for alts and other purposes.
+ * @property fableId - ID string to construct url leading to fable content.
  */
 export interface FableTileProps {
   imageUrl: string;
