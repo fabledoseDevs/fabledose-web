6af177b4d2b07dca178ca104bd88de19488d13b7
Paweł Modzelewski <last.dunadan@gmail.com>
2025-07-08 10:27:33 +0200

    Maintenance Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
 'use client';
-import type { ReactElement } from 'react';
+import { type ReactElement, useState } from 'react';
 
 import LandingPage from '@/components/templates/LandingPage';
-const Home = (): ReactElement => <LandingPage />;
+import Maintenance from '@/components/templates/Maintenance';
+
+const Home = (): ReactElement => {
+  const [showMaintenance, setShowMaintenance] = useState(true);
+
+  return (
+    <>
+      {showMaintenance ? <Maintenance /> : <LandingPage />}
+      <div
+        style={{
+          position: 'fixed',
+          bottom: '10px',
+          left: '10px',
+          width: '20px',
+          height: '20px',
+          cursor: 'pointer',
+          opacity: '0.1',
+          zIndex: 9999,
+        }}
+        onClick={() => setShowMaintenance(false)}
+      />
+    </>
+  );
+};
 
 export default Home;
---
+++ src/components/templates/Maintenance/index.ts
@@ -0,0 +1,2 @@
+export { Maintenance as default } from './Maintenance';
+export type { MaintenanceProps } from './Maintenance.types';
