c8161072371b2158b3e4c567855ab8f156993694
Paweł Modzelewski <last.dunadan@gmail.com>
2025-11-25 16:59:31 +0100

    login form and other components

---
+++ src/components/molecules/LoginForm/LoginForm.tsx
@@ -0,0 +1,84 @@
+import Link from 'next/link';
+
+import Button from '@/atoms/Button';
+import {
+  ACTION_TYPE,
+  BUTTON_VARIANT,
+  WIDTH_TYPE,
+} from '@/atoms/Button/Button.types';
+import Headline from '@/atoms/Headline';
+import {
+  FOREGROUND_COLOR,
+  HEADLINE_TYPE,
+} from '@/atoms/Headline/Headline.types';
+import InputField from '@/atoms/InputField';
+import { INPUT_TYPE } from '@/atoms/InputField/InputField.types';
+import Separator from '@/atoms/Separator';
+import { SEPARATOR_COLOR } from '@/atoms/Separator/Separator.types';
+
+import {
+  Form,
+  FormCard,
+  Label,
+  LabelText,
+  LoginFormBody,
+  NewAccount,
+  ResetPassword,
+} from './LoginForm.styled';
+import type { LoginForm as LoginFormType } from './LoginForm.types';
+
+export const LoginForm: LoginFormType = () => (
+  <LoginFormBody>
+    <FormCard>
+      <Headline weight={HEADLINE_TYPE.BIG} color={FOREGROUND_COLOR.PURPLE}>
+        Zaloguj się
+      </Headline>
+      <Form>
+        <Button
+          actionType={ACTION_TYPE.FUNCTION_TRIGGER}
+          variant={BUTTON_VARIANT.WHITE}
+          text={'Zaloguj się przez Google'}
+          width={{
+            widthType: WIDTH_TYPE.PERCENT,
+            widthValue: 100,
+          }}
+          iconUrl={'/icons/google.png'}
+        />
+        <Button
+          actionType={ACTION_TYPE.FUNCTION_TRIGGER}
+          variant={BUTTON_VARIANT.WHITE}
+          text={'Zaloguj się przez Apple'}
+          width={{
+            widthType: WIDTH_TYPE.PERCENT,
+            widthValue: 100,
+          }}
+          iconUrl={'/icons/apple.png'}
+        />
+        <Separator color={SEPARATOR_COLOR.GRAY} label={'lub'} />
+        <Label>
+          <LabelText>Adres email:</LabelText>
+          <InputField type={INPUT_TYPE.TEXT} placeholder={'twoj@adres.email'} />
+        </Label>
+        <Label>
+          <LabelText>Hasło:</LabelText>
+          <InputField type={INPUT_TYPE.PASSWORD} />
+        </Label>
+        <ResetPassword>
+          <Link href={'/'}>Nie pamiętam hasła</Link>
+        </ResetPassword>
+        <Button
+          actionType={ACTION_TYPE.SUBMIT}
+          variant={BUTTON_VARIANT.RED}
+          width={{
+            widthType: WIDTH_TYPE.PERCENT,
+            widthValue: 100,
+          }}
+          text={'Zaloguj'}
+        />
+      </Form>
+      <NewAccount>
+        Nie masz konta? <Link href={'/'}>Zarejestru się</Link>.
+      </NewAccount>
+    </FormCard>
+  </LoginFormBody>
+);
---
+++ src/components/templates/LoginPage/LoginPage.types.ts
@@ -0,0 +1,26 @@
+import type { ReactElement } from 'react';
+
+/**
+ * @module
+ * This file defines the types and interfaces for the LoginPage component.
+ */
+
+/**
+ * Component renders a maintenance page for the application.
+ * The maintenance page includes:
+ * - Jumbotron with headline about planned release
+ * - Informational paragraph about the site being under construction
+ * - Button linking to a demo site
+ *
+ * @group Component
+ *
+ * @example
+ * ```tsx
+ * <LoginPage />
+ * ```
+ */
+
+/**
+ * LoginPage component type definition
+ */
+export type LoginPage = () => ReactElement;
