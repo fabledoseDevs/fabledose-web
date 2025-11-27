0e3cf00e51da0664d3d6964e152e13683b2edc41
Paweł Modzelewski <last.dunadan@gmail.com>
2025-11-27 10:30:00 +0100

    register page for test purpose

---
+++ src/components/templates/RegisterPage/RegisterPage.styled.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const RegisterPageBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: white;
+`;
