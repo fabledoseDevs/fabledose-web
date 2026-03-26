74b02f883cb241261fd5a6d6e8bad1a247a5bf47
Paweł Modzelewski <last.dunadan@gmail.com>
2026-03-26 07:50:42 +0100

    Audiobook - v.i.p

--- src/components/templates/UserDesktop/UserDesktop.tsx
+++ src/components/templates/UserDesktop/UserDesktop.tsx
@@ -7,6 +7,7 @@ import {
   FLEX_ALIGNMENT,
   FLEX_DIRECTION,
 } from '@/atoms/Container/Container.types';
+import Audiobook, { audiobookMock } from '@/organisms/Audiobook';
 import DesktopTilesSlider from '@/organisms/DesktopTilesSlider';
 import desktopTilesSliderMock from '@/organisms/DesktopTilesSlider/DesktopTilesSlider.mock';
 import { FooterSimple } from '@/organisms/FooterSimple';
@@ -81,7 +82,6 @@ export const UserDesktop: UserDesktopType = () => {
             </Pagination>
           </EmblaViewport>
         </Container>
-
         <Container
           containerType={CONTAINER_ELEMENT.SECTION}
           backgroundColor={BACKGROUND_COLOR.PURPLE}
@@ -97,6 +97,16 @@ export const UserDesktop: UserDesktopType = () => {
             title={'Edukacyjne'}
             tiles={desktopTilesSliderMock.tiles}
           />
+        </Container>
+
+        <Container
+          containerType={CONTAINER_ELEMENT.SECTION}
+          backgroundColor={BACKGROUND_COLOR.PURPLE}
+          alignItems={FLEX_ALIGNMENT.CENTER}
+          justifyContent={FLEX_ALIGNMENT.CENTER}
+          flexDirection={FLEX_DIRECTION.COLUMN}
+        >
+          <Audiobook source={audiobookMock} onClose={() => undefined} />
         </Container>
       </UserDesktopBody>
       <FooterSimple />
